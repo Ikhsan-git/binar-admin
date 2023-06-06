abd34b8af15925986bd6330c58918806e6abc2ff
ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2023-06-06 23:19:32 +0700

    initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,122 @@
+import { UilAngleDown, UilBars, UilEstate, UilTruck } from '@iconscout/react-unicons'
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { hidden } from '../features/navbar/navbarSlice'
+import { Toaster } from 'react-hot-toast'
+import { access_token } from '../utils'
+import { useQuery } from '@tanstack/react-query'
+
+
+const Layout = ({ children }) => {
+
+  return (
+    <div className="layout">
+      <Sidebar />
+      <div className="page-content">{children}</div>
+      <Header />
+      <Toaster />
+    </div>
+  )
+}
+
+const Header = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [showProfile, setShowProfile] = useState(false)
+
+  // show profile detail
+  const handleShowProfile = () => {
+    setShowProfile(!showProfile)
+  }
+
+  // logout function
+  const handleLogout = () => {
+    setShowProfile(!showProfile)
+    localStorage.removeItem("token")
+    navigate("/login")
+  }
+
+  return (
+    <header className="header p-0">
+      <div className="header header-container">
+        <div className="header-left df-center">
+          <div className="header-logo"></div>
+          <button onClick={() => dispatch(hidden())} className="menu-icon df-center">
+            <UilBars />
+          </button>
+        </div>
+        <div className="header-main df-center">
+          <Search />
+          <div className="header-user">
+            <div onClick={handleShowProfile} className="user-profile df-center">
+              <div className="user-logo df-center">
+                <p>A</p>
+              </div>
+              <UilAngleDown />
+            </div>
+            {showProfile && (
+              <div className="user-detail">
+                <small>Signed in as admin</small>
+                <hr />
+                <div onClick={handleLogout} className="user-logout">
+                  <h5>Logout</h5>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+const Sidebar = () => {
+  return (
+    <aside className='sidebar'>
+      <div className='sidebar-logo df-center'>
+        <span />
+      </div>
+      <div className='sidebar-menu '>
+        <NavLink to={'/dashboard'}>
+          <div className='menu-dashboard df-center'>
+            <UilEstate />
+            <p>Dashboard</p>
+          </div>
+        </NavLink>
+        <NavLink to={'/cars'}>
+          <div className='menu-cars df-center'>
+            <UilTruck />
+            <p>Cars</p>
+          </div>
+        </NavLink>
+      </div>
+    </aside>
+  )
+}
+
+
+const Search = () => {
+  const [query, setQuery] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    navigate(`/cars/search/${query}`)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="header-search df-center">
+      <input type="search" placeholder="Search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button className="btn-outlined-primary">
+        Search
+      </button>
+    </form>
+  )
+}
+
+export default Layout
---
+++ src/page/Login.jsx
@@ -0,0 +1,115 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errMsg, setErrMsg] = useState(false)
+  const [logging, setLogging] = useState(false)
+
+  const navigate = useNavigate()
+
+  const user = localStorage.getItem('token')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setLogging(true)
+    setErrMsg(false)
+
+    const payload = {
+      email,
+      password,
+    }
+    await fetch('https://bootcamp-rent-cars.herokuapp.com/admin/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw Error(response.status)
+      })
+      .then((data) => {
+        localStorage.setItem('token', data.access_token)
+        setLogging(false)
+        navigate('/dashboard')
+      })
+      .catch((error) => {
+        setErrMsg(true)
+        console.error('Error:', error)
+      })
+  }
+
+  if (user) {
+    return navigate('/dashboard')
+  }
+
+  return (
+    <div className='signin'>
+      <div className='left'>
+        <img src='img/cover-login.png' alt='cover-login' />
+      </div>
+      <div className='right df-center'>
+        <div className='signin-container df-center'>
+          <div className='logo'>
+            <Link to='/'>
+              <h3></h3>
+            </Link>
+          </div>
+          <h2>Welcome Admin BCR!</h2>
+          {errMsg && <Error />}
+    
+          <form className='form-login' onSubmit={handleSubmit}>
+            <div className='signin-email'>
+              <label htmlFor='email'>Email</label>
+              <input
+                id='email'
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                placeholder='Contoh: johndee@gmail.com'
+                type='text'
+                autoComplete='off'
+                required
+              />
+            </div>
+            <div className='signin-password'>
+              <label htmlFor='password'>Password</label>
+              <input
+                id='password'
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type='password'
+                placeholder='6+ karakter'
+                required
+              />
+            </div>
+            <button className='btn-primary'>Sign In</button>
+          </form>
+
+          {logging && !errMsg && (
+            <div>
+              <p>Signing...</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Error = () => {
+  return (
+    <div className='error-login-card d-center'>
+      <p>
+        Masukkan username dan password yang benar. Perhatikan penggunaan huruf
+        kapital.
+      </p>
+    </div>
+  )
+}
+
+export default Login
